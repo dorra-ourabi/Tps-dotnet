d6b59daf82a885943b8ea76fdbf0b0e52fb8fc9c
dorra dorra <dorra.ourabi@insat.ucar.tn>
2026-02-13 16:32:29 +0100

    added tp2

---
+++ tp2/Controllers/MovieController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tp2.Models;
+
+namespace tp2.Controllers
+{
+    [Route("Movie")]
+    public class MovieController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Injection du DbContext
+        public MovieController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("Index")]
+        public IActionResult Index(int pageNumber = 1)
+        {
+            int pageSize = 5; // Nombre de films par page
+
+            var movies = _context.movies
+                .OrderBy(m => m.Name); // Tri alphabétique
+
+            // Pagination
+            var pagedMovies = movies
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            // Pour afficher la pagination dans la vue
+            ViewData["CurrentPage"] = pageNumber;
+            ViewData["TotalPages"] = (int)Math.Ceiling((double)_context.movies.Count() / pageSize);
+
+            return View(pagedMovies);
+        }
+        // GET: /Genre/Index/1
+        [HttpGet("Edit")]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        // POST: /Genre/Create
+        [HttpPost("Edit")]
+        [ValidateAntiForgeryToken] // <-- validation anti-XSS / CSRF
+        public IActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.movies.Add(movie);
+                _context.SaveChanges();
+                return RedirectToAction("Index"); // retour à la liste
+            }
+
+            return View(movie); // si la validation échoue
+        }
+        // GET: /Movie/Delete/5
+        [HttpGet("Delete/{name}")]
+        public IActionResult Delete(string name)
+        {
+            var movie = _context.movies.FirstOrDefault(m => m.Name == name);
+            if (movie == null)
+                return NotFound(); // 404 if movie doesn't exist
+
+            return View(movie); // show a confirmation page
+        }
+        
+
+    }
+}
---
+++ tp2/Models/ApplicationDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+namespace tp2.Models
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = "server=localhost;port=3307;database=mydb;user=root;password=;";
+
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8,0,33)));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
